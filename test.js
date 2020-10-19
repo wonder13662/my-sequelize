740d6bcbd3f5de4da422cc1dfe45f2ba8aaa918d
wonder13662 <wonder13662@gmail.com>
2020-10-19 17:40:22 +0900

    Add sample file

--- test.js
+++ test.js
@@ -1,8 +1,33 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
-(() => {
-  // Example for postgres
-  const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
-})
+// https://sequelize.org/master/manual/getting-started.html
+
+const func = () => {
+  const { Sequelize, Model, DataTypes } = require("sequelize");
+  const sequelize = new Sequelize("sqlite::memory:");
+
+  const User = sequelize.define("user", {
+    username: DataTypes.TEXT,
+    isAdmin: DataTypes.BOOLEAN,
+    name: DataTypes.TEXT,
+    favoriteColor: {
+      type: DataTypes.TEXT,
+      defaultValue: 'green'
+    },
+    age: DataTypes.INTEGER,
+    cash: DataTypes.INTEGER
+  });
+
+  (async () => {
+    await sequelize.sync({ force: true });
+    // Code here
+
+    // Create a new user
+    const jane = await User.create({ name: "Jane" });
+    console.log("Jane's auto-generated ID:", jane.id);
+    console.log("jane.name:", jane.name);
+  })();
+}
+func()
 
 
